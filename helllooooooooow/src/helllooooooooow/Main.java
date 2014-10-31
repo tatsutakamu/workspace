1592d0e9524a0d1345aa919855d605bf413d9a8b
tatsutakamu <tatsu.takamu@gmail.com>
2014-10-31 16:16:52 +0900

    wowowwwwwwwwwww

---
+++ helllooooooooow/src/helllooooooooow/Main.java
@@ -0,0 +1,12 @@
+package helllooooooooow;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("eclipse test");
+
+	}
+
+}
